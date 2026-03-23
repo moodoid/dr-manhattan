5b4a7ba3e15cc2aa12c3f7add197908d82fde6a4
megamuskeln <anoble447@gmail.com>
2026-03-23 11:43:26 +0000

    add new generic positions dataclass and function to parse

--- dr_manhattan/models/position.py
+++ dr_manhattan/models/position.py
@@ -2,6 +2,35 @@ from dataclasses import dataclass
 
 
 @dataclass
+class GenericPosition:
+    proxy_wallet: str
+    asset: str
+    condition_id: str
+    size: float
+    average_price: float
+    initial_value: float
+    current_value: float
+    cash_pnl: float
+    percent_pnl: float
+    total_bought: float
+    realized_pnl: float
+    percent_realized_pnl: float
+    current_price: float
+    redeemable: bool
+    mergable: bool
+    title: str
+    slug: str
+    icon: str
+    event_id: str
+    event_slug: str
+    outcome: str
+    outcome_index: int 
+    opposition_outcome: str
+    opposition_asset: str
+    end_date: str
+    negative_risk: bool
+    
+@dataclass
 class Position:
     """Represents a position in a prediction market"""
 
