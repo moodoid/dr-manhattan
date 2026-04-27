e4c6c7e61ea77ef57b03cd38969d7b8b987c014d
= <=>
2026-04-27 16:53:02 +0000

    bump to py-clob-client-v2

--- dr_manhattan/exchanges/__init__.py
+++ dr_manhattan/exchanges/__init__.py
@@ -2,13 +2,11 @@ from .kalshi import Kalshi
 from .limitless import Limitless
 from .opinion import Opinion
 from .polymarket import Polymarket
-from .polymarket.polymarket_builder import PolymarketBuilder
 from .polymarket.polymarket_operator import PolymarketOperator
 from .predictfun import PredictFun
 
 __all__ = [
     "Polymarket",
-    "PolymarketBuilder",
     "PolymarketOperator",
     "Limitless",
     "Opinion",
--- dr_manhattan/exchanges/polymarket_builder.py
+++
@@ -1,9 +0,0 @@
-"""Backward-compatible import for PolymarketBuilder.
-
-This module preserves the legacy import path:
-    dr_manhattan.exchanges.polymarket_builder
-"""
-
-from .polymarket.polymarket_builder import PolymarketBuilder
-
-__all__ = ["PolymarketBuilder"]
